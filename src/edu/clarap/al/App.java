35755fe20a21471acd984743e4a3fd7bc7782f25
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-07-19 13:51:56 -0500

    Probando rama de nuevo

--- src/edu/clarap/al/App.java
+++ src/edu/clarap/al/App.java
@@ -1,6 +1,6 @@
 package edu.clarap.al;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World (v191351)!!!!!!");
     }
 }
