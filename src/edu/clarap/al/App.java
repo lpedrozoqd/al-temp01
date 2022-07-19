9eabfe563f6c4daade3d33c82c0abf4845be1be1
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-07-19 13:50:25 -0500

    Probando rama

--- src/edu/clarap/al/App.java
+++ src/edu/clarap/al/App.java
@@ -1,6 +1,6 @@
 package edu.clarap.al;
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
